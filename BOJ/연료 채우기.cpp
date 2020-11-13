8f2afb3e9bce05da74e950a7c5f33e799025e2d8
hakjun <kimhj9503@naver.com>
2020-11-13 16:42:06 +0900

    BOJ_1826(Greedy)

---
+++ BOJ/연료 채우기.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<queue>
+
+#define pii pair<int, int>
+using namespace std;
+
+int N, a, b, dest, fuel;
+int ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	priority_queue<pii, vector<pii>, greater<pii> > pq;
+	priority_queue<int> temp;
+
+	cin >> N;
+	while (N--) {
+		cin >> a >> b;
+		pq.push({ a, b });
+	}
+	cin >> dest >> fuel;
+
+	while (fuel < dest) {
+		while (!pq.empty()) {
+			if (fuel < pq.top().first) break;
+			temp.push(pq.top().second);
+			pq.pop();
+		}
+
+		if (temp.empty()) {
+			ans = -1;
+			break;
+		}
+
+		fuel += temp.top();
+		temp.pop();
+		ans++;
+	}
+
+	cout << ans;
+	return 0;
+}
