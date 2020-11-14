1daf28f3df3a320f09a23e49115ee218470080e1
hakjun <kimhj9503@naver.com>
2020-11-14 23:58:09 +0900

    BOJ_5639(Binary Search Tree)

---
+++ BOJ/이진 검색 트리.cpp
@@ -0,0 +1,60 @@
+﻿// Tree를 만든 후 후위 순회하는 방법
+
+#include<iostream>
+
+using namespace std;
+
+struct NODE {
+	int key;
+	NODE* left;
+	NODE* right;
+
+	NODE(int val) : key(val), left(NULL), right(NULL) {}
+	void setLeft(NODE* newNode) {
+		left = newNode;
+	}
+	void setRight(NODE* newNode) {
+		right = newNode;
+	}
+
+};
+
+NODE* insertNode(NODE* root, NODE* newNode) {
+	if (root == NULL) {
+		root = newNode;
+	}
+	else {
+		if (root->key > newNode->key) {
+			root->setLeft(insertNode(root->left, newNode));
+		}
+		else {
+			root->setRight(insertNode(root->right, newNode));
+		}
+	}
+
+	return root;
+}
+
+void print_post(NODE* cur) {
+	if (cur == NULL) return;
+
+	print_post(cur->left);
+	print_post(cur->right);
+	cout << cur->key << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	NODE* tree = NULL;
+	int inp;
+
+	while (cin >> inp) {
+		tree = insertNode(tree, new NODE(inp));
+	}
+
+	print_post(tree);
+
+	return 0;
+}
