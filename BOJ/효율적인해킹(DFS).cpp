f6d5cebb1098207d134003a577743b51ea1c7897
hakjun <kimhj9503@naver.com>
2020-11-29 16:13:15 +0900

    BOJ_1325(DFS)

---
+++ BOJ/효율적인해킹(DFS).cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+
+int N, M, a, b, maxVal;
+vector<int> info[10001];
+int dp[10001];
+bool visit[10001];
+
+int dfs(int cur) {
+	visit[cur] = true;
+
+	int cnt = 1;
+	for (int i = 0; i < info[cur].size(); i++) {
+		int next = info[cur][i];
+		if (!visit[next]) {
+			cnt += dfs(next);			
+		}
+	}
+
+	return cnt;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> a >> b;
+		info[b].push_back(a);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		dp[i] = dfs(i);
+
+		if (maxVal < dp[i]) {
+			maxVal = dp[i];
+		}
+		memset(visit, false, sizeof(visit));
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (dp[i] == maxVal) {
+			cout << i << " ";
+		}
+	}
+
+	return 0;
+}
